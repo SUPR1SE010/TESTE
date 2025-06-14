61e0fa71ce2d1eba77bd525258c0cca2786d8ee5
SUPR1SE010 <robarbosa6195385@gmail.com>
2025-06-14 16:35:07 +0000

    amem

--- like/src/pages/api/index.js
+++ like/src/pages/api/index.js
@@ -1,5 +1,14 @@
 async function api(req,res) {
-    res.status(200).json({
+    console.log("recebido: ",req.query)
+    var x = req.query.x
+    var y = req.query.y
+    var test = x>y? true:false;
+    if (test== true){
+        return res.status(200).json({
+            status: "deu serto"
+        })
+    }
+    return res.status(400).json({
         status:"chapolin colorado"
     })
 }
